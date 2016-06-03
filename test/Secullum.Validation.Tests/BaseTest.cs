c36c4ad53de89dccef4375f4babb67af8e6ae2b4
fhelwanger <fhelwanger@gmail.com>
2016-06-03 09:30:33 -0300

    Include net451 in frameworks

--- test/Secullum.Validation.Tests/BaseTest.cs
+++ test/Secullum.Validation.Tests/BaseTest.cs
@@ -6,8 +6,13 @@ namespace Secullum.Validation.Tests
     {
         protected void SetCurrentThreadCulture(CultureInfo cultureInfo)
         {
+#if NET451
+            System.Threading.Thread.CurrentThread.CurrentCulture = cultureInfo;
+            System.Threading.Thread.CurrentThread.CurrentUICulture = cultureInfo;
+#else
             CultureInfo.CurrentCulture = cultureInfo;
             CultureInfo.CurrentUICulture = cultureInfo;
+#endif
         }
     }
 }
