23092d5701ee30b05643ecec5f1da2f142c778ad
fhelwanger <fhelwanger@gmail.com>
2017-05-11 16:03:11 -0300

    Add CamelCase option

--- src/Secullum.Validation/ValidationError.cs
+++ src/Secullum.Validation/ValidationError.cs
@@ -2,8 +2,32 @@
 {
     public class ValidationError
     {
-        public string Property { get; }
-        public string Message { get; }
+        private string _property;
+
+        public static bool CamelCase { get; set; }
+
+        public string Property
+        {
+            get
+            {
+                return _property;
+            }
+            set
+            {
+                if (CamelCase)
+                {
+                    _property = value != null
+                        ? value.Substring(0, 1).ToLower() + value.Substring(1)
+                        : null;
+                }
+                else
+                {
+                    _property = value;
+                }
+            }
+        }
+
+        public string Message { get; set; }
 
         public ValidationError(string property, string message)
         {
---
+++ test/Secullum.Validation.Tests/ValidationErrorTests.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+
+namespace Secullum.Validation.Tests
+{
+    public class ValidationErrorTests
+    {
+        [Fact]
+        public void ValidationError_PascalCase_GivenPropertyAndMessage_SetIt()
+        {
+            // Arrange
+            ValidationError.CamelCase = false;
+
+            // Act
+            var error = new ValidationError("Name", "Invalid name.");
+
+            // Assert
+            Assert.Equal("Name", error.Property);
+        }
+
+        [Fact]
+        public void ValidationError_CamelCase_GivenPropertyAndMessage_SetIt()
+        {
+            // Arrange
+            ValidationError.CamelCase = true;
+
+            // Act
+            var error = new ValidationError("Name", "Invalid name.");
+
+            // Assert
+            Assert.Equal("name", error.Property);
+        }
+    }
+}
